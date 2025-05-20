a273fb1b4401c6712342680195821ff990d8cfea
Nilsu Acar <nilsuacar97@gmail.com>
2025-05-21 00:27:09 +0300

    add type

---
+++ src/types/userdata.types.ts
@@ -0,0 +1,6 @@
+export interface UserDataType {
+  name: string;
+  mailReceivedDate: string | null | undefined;
+  solutionSentDate?: string | null | undefined;
+  isBackgroundColorRed?: boolean | undefined | null;
+}
